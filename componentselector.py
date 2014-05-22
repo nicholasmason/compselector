6e692eee55768ced0cccbeb114d0db667d001066
nicholasmason <nicholas.albert.mason@gmail.com>
2014-05-22 17:11:43 -0400

    initial project version

---
+++ componentselector.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import sys,getopt
+
+# Store input and output file names
+ifile=''
+ofile=''
+
+# Read command line args
+myopts, args = getopt.getopt(sys.argv[1:],"i:o:")
+
+###############################
+# o == option
+# a == argument passed to the o
+###############################
+for o, a in myopts:
+    if o == '-i':
+        ifile=a
+    elif o == '-o':
+        ofile=a
+    else:
+        print("Usage: %s -i input -o output" % sys.argv[0])
+ 
+
+
