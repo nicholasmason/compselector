89b91c62fea16fccf1a9ab4c651d54575ad06d5b
nicholasmason <nicholas.albert.mason@gmail.com>
2014-05-28 14:10:03 -0400

    Add v2 and README

--- componentselector.py
+++ componentselector.py
@@ -1,25 +1,68 @@
 #!/usr/bin/python
-
-import sys,getopt
+import re
+from sys import argv
 
 # Store input and output file names
-ifile=''
-ofile=''
+script,ifile,ofile = argv
 
-# Read command line args
-myopts, args = getopt.getopt(sys.argv[1:],"i:o:")
+o = open(ofile,'w')
 
-###############################
-# o == option
-# a == argument passed to the o
-###############################
-for o, a in myopts:
-    if o == '-i':
-        ifile=a
-    elif o == '-o':
-        ofile=a
-    else:
-        print("Usage: %s -i input -o output" % sys.argv[0])
- 
+comps = {}
+comps_index= []
 
+with open(ifile) as f:
+    i = 0
+    cc = 0
+    for line in f:
+        if re.search(">", line):
+            cc += 1
+            if cc % 1000 == 0:
+                print cc
+            comps_index.append(i)
+            foo = line.split(" ")[0].split("_seq")[0]
+            bar = int(line.split(" ")[1].split("=")[1])
+            if foo in comps.keys():
+                if(bar > comps[foo].keys()[0]):
+                    comps[foo] = {bar:i}
+            else:
+                comps[foo] = {bar:i}
+                    
+        i+=1
 
+print "Done parsing " + str(cc) + " components."
+
+print_index=[]
+for k in comps:
+    print_index.append(comps[k].values()[0])
+
+print "Completed indexing step 1."
+
+disp_index=[]
+
+for j in xrange(0,len(comps_index)):
+    if j % 1000 == 0:
+        print j    
+    if comps_index[j] in print_index:
+        if j==(len(comps_index)-1):
+            disp_index.extend(range(comps_index[j],i))
+        else:
+            disp_index.extend(range(comps_index[j],comps_index[j+1]))
+
+print "Completed indexing step 2."
+
+with open(ifile) as f:
+    i = 0
+    cc = 0
+    for line in f:
+        if cc % 1000 == 0:
+            print cc
+        if i in disp_index:
+            if re.search(">",line):
+                o.write(line.split(" ")[0].split("_seq")[0])
+                o.write("\n")
+            else:
+                o.write(line)
+            cc+=1
+        i+=1
+
+o.close()
